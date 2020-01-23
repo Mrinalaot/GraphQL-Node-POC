359dc247addf5faf05e9c26ff9ac1a88bcb931b8
Maniteja Bonthu <maniteja.bonthu@thomsonreuters.com>
2020-01-23 13:59:13 +0530

    Implemented Query,Mutations for Users,Posts,Comments

--- src/db.js
+++ src/db.js
@@ -16,9 +16,48 @@ const users = [
         email: "ravi@example.com"
     }
 ];
+const posts = [
+    {
+        id: "10",
+        title: "GraphQL 101",
+        body: "This is how to use GraphQL with Node.js...",
+        published: true,
+        author: "1"
+    },
+    {
+        id: "11",
+        title: "GraphQL 201",
+        body: "This is how to use GraphQL with React...",
+        published: false,
+        author: "1"
+    },
+    {
+        id: "12",
+        title: "GraphQL 301",
+        body: "This is how to use GraphQL with Angular...",
+        published: true,
+        author: "2"
+    }
+];
 
+const comments = [
+    {
+        id: "102",
+        text: "This worked well for me. Thanks!",
+        author: "1",
+        post: "10"
+    },
+    {
+        id: "104",
+        text: "This did no work.",
+        author: "2",
+        post: "12"
+    }
+];
 const db = {
-    users
+    users,
+    posts,
+    comments
 };
 
 export { db as default };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import { GraphQLServer } from "graphql-yoga";
 import db from "./db";
 import Query from "./resolvers/Query";
-
+import Mutation from './resolvers/Mutation'
 const PORT = process.env.PORT || 5000
 const server = new GraphQLServer({
     typeDefs: "./src/schema.graphql",
     resolvers: {
-        Query
+        Query,
+        Mutation
     },
     context: {
         db
